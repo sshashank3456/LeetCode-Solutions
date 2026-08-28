0a091d2854210ef2baee3ad06af7bda10631f9cd
Shashank Kumar <149873639+sshashank3456@users.noreply.github.com>
2026-08-28 17:39:12 +0530

    update: Max Consecutive Ones III solution

--- Max Consecutive Ones III.py
+++ Max Consecutive Ones III.py
@@ -1,18 +1,15 @@
-            if nums[high] in freq:
-                freq[nums[high]]+=1
-            else:
-                freq[nums[high]]= 1
-            maxcnt= max(freq.values())
-            while(high-low+1 - maxcnt > k):
-                freq[nums[low]]-=1
-                if freq[nums[low]]==0:
-        for high in range(n):
-        maxcnt=0
-        freq={}
-        res=0
-        low=0
-        n=len(nums)
-    def longestOnes(self, nums: List[int], k: int) -> int:
 class Solution:
-        if k==0:
-            return 0
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        n=len(nums)
+        low=0
+        zero=0
+        for high in range(n):
+            if nums[high]==0:
+                zero+=1
+            while( zero> k):
+                if nums[low]==0:
+                    zero-=1
+                low+=1
+        res=0
+            res= max(res, high-low+1)
+        return res
