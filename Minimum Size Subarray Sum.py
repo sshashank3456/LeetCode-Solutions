bd2ba05fe1b39c44528a386465efa7a67ba9cb36
Shashank Kumar <149873639+sshashank3456@users.noreply.github.com>
2026-08-28 15:10:57 +0530

    update: Minimum Size Subarray Sum solution

--- Minimum Size Subarray Sum.py
+++ Minimum Size Subarray Sum.py
@@ -1,18 +1,15 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        low=0
-        high=0
-        sumof=0
         n=len(nums)
-        while(high<n):
-            while(sumof>=target):
-                length=high-low+1
-        res=float('inf')
-                res= min(res, length)
-                sumof=sumof-nums[low]
+        low=0
+        sum=0
+        res= float('inf')
+        for high in range(n):
+            sum= sum + nums[high]
+            while sum >= target:
+                sum= sum - nums[low]
                 low+=1
-            high+=1
-            sumof=sumof+ nums[high]
-        if res == float('inf'):
+        if res== float('inf'):
             return 0
+                res= min(res, high-low +1)
         return res
