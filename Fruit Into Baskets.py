2a17ffb4221f7ba7b50b71d543dd66e0e737a35a
Shashank Kumar <149873639+sshashank3456@users.noreply.github.com>
2026-08-28 15:38:18 +0530

    update: Fruit Into Baskets solution

--- Fruit Into Baskets.py
+++ Fruit Into Baskets.py
@@ -1,17 +1,17 @@
-    def totalFruit(self, fruits: List[int]) -> int:
-        n=len(fruits)
-        low=0
-        res=0
         freq={}
         for high in range(n):
             if fruits[high] in freq:
                 freq[fruits[high]]+=1
             else:
-                freq[fruits[high]]= 1
-            while(len(freq)>2):
+                freq[fruits[high]] = 1
+            while (len(freq)> 2):
                 freq[fruits[low]]-=1
-                if freq[fruits[low]]==0:
+                if freq[fruits[low]]== 0:
                     del freq[fruits[low]]
                 low+=1
-            res= max(res, high-low+1)
+            res= max(res, high- low + 1)
+        res=0
+        low=0
+        n= len(fruits)
+    def totalFruit(self, fruits: List[int]) -> int:
         return res
