1347d7e685a866a3126322231307871cc283573a
Shashank Kumar <149873639+sshashank3456@users.noreply.github.com>
2026-08-28 17:30:38 +0530

    update: Longest Repeating Character Replacement solution

--- Longest Repeating Character Replacement.py
+++ Longest Repeating Character Replacement.py
@@ -1,13 +1,17 @@
+    def characterReplacement(self, s: str, k: int) -> int:
+        n=len(s)
+        low=0
+        res=0
+        freq={}
         for high in range(n):
-            f[ord(s[high])]+=1
-            maxcnt= max(f)
-            length= high-low+1
-            diff=length- maxcnt
-            while(diff>k):
-                f[ord(s[low])]-=1
+            if s[high] in freq:
+                freq[s[high]]+=1
+            else:
+                freq[s[high]]=1
+        maxcnt=0
+            maxcnt= max(freq.values())
+            while((high-low+1) - maxcnt > k):
+                freq[s[low]]-=1
                 low+=1
-                maxcnt= max(f)
-                length= high-low+1
-                diff=length- maxcnt
             res= max(res, high-low+1)
         return res
