b6cf3fbbc2bbe288fbc2cdbf5e19d40788ab3730
Shashank Kumar <149873639+sshashank3456@users.noreply.github.com>
2026-08-29 13:10:19 +0530

    update: Happy Number solution

--- Happy Number.py
+++ Happy Number.py
@@ -1,15 +1,17 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def getno(n):
-            total=0
+        def sqr(n):
+            total= 0
             while n > 0:
                 ld= n%10
-                total= total+ ld **2
-                n //= 10
+                total = total+ ld*ld
+                n= n//10
             return total
-        slow=n
-        fast=getno(n)
-        while slow != fast:
-            slow=getno(slow)
-            fast=getno(getno(fast))
-        return slow== 1
+        slow= sqr(n)
+        fast= sqr(n)
+        while (fast != 1):
+            slow=sqr(slow)
+            fast=sqr(sqr(fast))
+            if slow== fast and slow != 1:
+                return False
+        return True
