6863a4cd9ac7e7f71c01b55dc8cc76b283034ecc
Shashank Kumar <149873639+sshashank3456@users.noreply.github.com>
2026-08-31 12:34:38 +0530

    update: Maximum Absolute Sum of Any Subarray solution

--- Maximum Absolute Sum of Any Subarray.py
+++ Maximum Absolute Sum of Any Subarray.py
@@ -1,16 +1,13 @@
-    def maxAbsoluteSum(self, nums: List
-    [int]) -> int:
+class Solution:
+    def maxAbsoluteSum(self, nums: List[int]) -> int:
+        n= len(nums)
         minsum=nums[0]
-        maxsum= nums[0]
+        maxsum=nums[0]
         res=abs(nums[0])
-        for i in range(1, len(nums)):
+        for i in range(1, n):
             prevminsum= minsum
             prevmaxsum= maxsum
-            minsum= minsum + nums[i]
-            maxsum= maxsum+ nums[i]
-            v1= min(minsum, nums[i])
-            v2= max(maxsum, nums[i])
-            res= max(res, abs(v1), abs(v2))
+            minsum= min(prevminsum + nums[i], nums[i])
+            maxsum= max(prevmaxsum+ nums[i], nums[i])
+            res= max(res, max(abs(minsum), abs(maxsum)))
         return res
-            minsum= v1
-            maxsum= v2
