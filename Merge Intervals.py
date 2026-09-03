9aecc9a002103d975fc8e7a0c4ffeccb8ad00449
Shashank Kumar <149873639+sshashank3456@users.noreply.github.com>
2026-09-03 13:05:05 +0530

    update: Merge Intervals solution

--- Merge Intervals.py
+++ Merge Intervals.py
@@ -1,17 +1,20 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        n=len(intervals)
+        res=[]
+        start1= intervals[0][0]
+        end1= intervals[0][1]
+        for i in range(n):
             start2= intervals[i][0]
             end2= intervals[i][1]
             if end1 >= start2:
-                start1=start1
+                start1= start1
                 end1= max(end1, end2)
-                continue
             else:
-                start1= start2
-                end1=end2
                 res.append([start1, end1])
-        for i in range(1, len(intervals)):
-        end1= intervals[0][1]
-        start1= intervals[0][0]
-        res=[]
-        intervals.sort()
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-class Solution:
+        res.append([start1, end1])
+                continue
+                start1=start2
+                end1=end2
+        return res
