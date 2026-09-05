f2fdf6749f966026e50ef5f0a0d07566ff30d056
Shashank Kumar <149873639+sshashank3456@users.noreply.github.com>
2026-09-05 20:32:14 +0530

    update: Two Sum solution

--- Two Sum.py
+++ Two Sum.py
@@ -1,6 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: 
-    int) -> List[int]:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen= {}
         for i in range(len(nums)):
             complement= target- nums[i]
