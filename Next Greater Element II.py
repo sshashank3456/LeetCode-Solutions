4d392578567e1939cf8f1028c2751fb35a8a8be3
Shashank Kumar <149873639+sshashank3456@users.noreply.github.com>
2026-09-10 19:22:34 +0530

    update: Next Greater Element II solution

--- Next Greater Element II.py
+++ Next Greater Element II.py
@@ -4,10 +4,10 @@ class Solution:
         res= [-1] * n
         st= []
         for i in range(2* n-1, -1, -1):
+            while st and st[-1] <= nums[i % n]:
+                st.pop()
             if i < n:
-            st.append(nums[i %n])
                 if st:
                     res[i] = st[-1]
-                st.pop()
-            while st and st[-1] <= nums[i % n]:
+            st.append(nums[i %n])
         return res
