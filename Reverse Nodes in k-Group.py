fc78bdc5e428919e60de3bae0228538ca6379122
Shashank Kumar <149873639+sshashank3456@users.noreply.github.com>
2026-09-16 13:15:31 +0530

    feat: Reverse Nodes in k-Group solution

---
+++ Reverse Nodes in k-Group.py
@@ -0,0 +1,17 @@
+                right= right.next
+                    break
+                if right is None:
+            for i in range(size-1):
+            right= left
+        while True:
+        size= k
+        prevleft = None
+        res= None
+        right = None
+        left= head
+            return None
+        if head is None:
+    def reverseKGroup(self, head: ListNode | None, k: int) -> ListNode | None:
+class Solution:
+#         self.next = next
+#         self.val = val
