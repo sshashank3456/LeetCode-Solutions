b5e857edb405411bd501e522d10a440a27060787
Shashank Kumar <149873639+sshashank3456@users.noreply.github.com>
2026-09-17 18:48:39 +0530

    feat: Binary Search solution

---
+++ Binary Search.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        low = 0
+        high= len(nums) -1
+        while low <= high:
+            guess= low+(high-low) // 2
+            if nums[guess] == target:
+                return guess
+            if nums[guess] < target:
+                low= guess +1
+            else:
+                high= guess -1
+        return -1
