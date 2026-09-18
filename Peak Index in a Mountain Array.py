a37d8d25dfa0cb3406cb0ad9832ebf2b60cfb06d
Shashank Kumar <149873639+sshashank3456@users.noreply.github.com>
2026-09-18 11:12:38 +0530

    feat: Peak Index in a Mountain Array solution

---
+++ Peak Index in a Mountain Array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: list[int]) -> int:
+        low= 0
+        high= len(arr) - 1
+        while low <= high:
+            guess= (low+ high) // 2
+            if arr[guess] < arr[guess+1]:
+                low= guess + 1
+            else:
+                high= guess -1
+            return low
