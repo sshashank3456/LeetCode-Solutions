cf46a24b7cac1cf1062e42761676eaca5916d4d5
Shashank Kumar <149873639+sshashank3456@users.noreply.github.com>
2026-09-19 10:48:10 +0530

    update: Search in Rotated Sorted Array solution

--- Search in Rotated Sorted Array.py
+++ Search in Rotated Sorted Array.py
@@ -1,6 +1,13 @@
-class Solution:
-    def search(self, nums: list[int], target: int) -> int:
-        for i in range(len(nums)):
-            if nums[i]== target:
-                return i
+                    if nums[0] > target:
+                        low= guess + 1
+                    else:
+                        high= guess - 1
+            else:
+                if nums[guess] > target:
+                    high= guess - 1
+                else:
+                    if nums[n-1] < target:
+                        high= guess -1
+                    else:
+                        low= guess + 1
         return -1
